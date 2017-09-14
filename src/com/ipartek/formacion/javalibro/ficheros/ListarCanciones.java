4ca4d00ac66bda8c1a74863a13573e07aa94b8d7
Administrador <Administrador@Formacion-TOS11.ipartekaula.com>
2017-09-14 13:16:43 +0200

    Ver los archivos de un fichero e imprimirlo

--- src/com/ipartek/formacion/javalibro/ficheros/ListarCanciones.java
+++ src/com/ipartek/formacion/javalibro/ficheros/ListarCanciones.java
@@ -9,12 +9,23 @@ import java.io.File;
 
 public class ListarCanciones {
 
-	
+	private static void listarDirectorio(File directorio, String separador) {
+		File[] ficheros=directorio.listFiles();
+		for(int x=0; x<ficheros.length;x++) {
+			System.out.println(separador +ficheros[x].getName());
+			if(ficheros[x].isDirectory()) {
+				String nuevo_separador;
+				nuevo_separador=separador +"-------";
+				listarDirectorio(ficheros[x], nuevo_separador);
+			}
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		
+	//No funciona si es compleja la estructura de la carpeta
+	/*	
 		File directorio= new File("C:\\desarrollo\\Music");
 		File [] lista =directorio.listFiles();
 		System.out.println("Directorios de Musica en la carpeta Music:");
@@ -29,6 +40,10 @@ public class ListarCanciones {
 			}
 		
 		}
+	
+	*/
+	File f=new File("C:\\desarrollo\\Music");
+	listarDirectorio(f, "-----");
 	}
 
 }
