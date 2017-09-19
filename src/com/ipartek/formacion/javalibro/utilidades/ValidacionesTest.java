20858ee0231defaaaf55c28f0f232066c3a520bf
Administrador <Administrador@Formacion-TOS11.ipartekaula.com>
2017-09-19 14:30:21 +0200

    Expresiones Regulares y validacion de DNI y email

---
+++ src/com/ipartek/formacion/javalibro/utilidades/ValidacionesTest.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.javalibro.utilidades;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ValidacionesTest extends TestCase {
+
+	public void testEmail() {
+		
+		
+		assertFalse(Validaciones.email(null));
+		assertFalse(Validaciones.email(""));
+		assertFalse(Validaciones.email("auraga.ipartek.com"));
+		assertFalse(Validaciones.email("auraga.ipartek."));
+		assertFalse(Validaciones.email("auraga@"));
+		assertFalse(Validaciones.email("auraga@ipartekcom"));
+		assertFalse(Validaciones.email("auraga@ipartekcom.e"));
+
+		assertTrue(Validaciones.email("auraga@ipartek.com"));
+
+	}
+	public void testDNI() {
+		assertFalse(Validaciones.dni(null));
+		assertFalse(Validaciones.dni(""));
+		assertFalse(Validaciones.dni("2eee"));
+		assertFalse(Validaciones.dni("1111111"));
+		assertFalse(Validaciones.dni("11111111Y"));
+		
+		assertTrue(Validaciones.dni("11111111H"));
+	}
+}
