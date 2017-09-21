e8ecc8f0b50bee0f4ed8698946abec249c75ce4d
Administrador <Administrador@Formacion-TOS11.ipartekaula.com>
2017-09-21 12:04:18 +0200

    Creacion de xml canciones.xml Creacion de clase LeerCancionesXML para poder leer de xml

--- src/com/ipartek/formacion/javalibro/pojo/Canciones.java
+++ src/com/ipartek/formacion/javalibro/pojo/Canciones.java
@@ -1,60 +1,62 @@
 package com.ipartek.formacion.javalibro.pojo;
 
-
 public class Canciones {
 
 	private String nombre;
 	private String artista;
 	private String duracion;
-	
-	
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Canciones(String nombre, String artista, String duracion, long id) {
+		super();
+		this.nombre = nombre;
+		this.artista = artista;
+		this.duracion = duracion;
+		this.id = -1;
+	}
+
 	public Canciones(String nombre, String artista, String duracion) {
 		super();
 		this.nombre = nombre;
 		this.artista = artista;
 		this.duracion = duracion;
 	}
-	
 
 	@Override
 	public String toString() {
-		return "Nombre :" + nombre + ", Artista :" + artista + ", Duracion: " + duracion ;
+		return "Canciones [nombre=" + nombre + ", artista=" + artista + ", duracion=" + duracion + ", id=" + id + "]";
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 
 	public String getArtista() {
 		return artista;
 	}
 
-
 	public void setArtista(String artista) {
 		this.artista = artista;
 	}
-
 
 	public String getDuracion() {
 		return duracion;
 	}
 
-
 	public void setDuracion(String duracion) {
 		this.duracion = duracion;
 	}
-
-
-	
-
-
-	
 
 }
---
+++ src/com/ipartek/formacion/javalibro/xml/LeerCancionesXML.java
@@ -0,0 +1,85 @@
+package com.ipartek.formacion.javalibro.xml;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.ipartek.formacion.javalibro.pojo.Canciones;
+
+/*
+ * Ejercicio para aprender a leer o parsear un documento XML
+ */
+
+public class LeerCancionesXML {
+	
+	static final String CANCIONES_XML="C:\\Users\\Administrador\\eclipse-workspace\\LibroJava\\src\\data\\canciones.xml";
+	static ArrayList<Canciones> canciones= null;
+	
+	
+	public static void main(String[] args) {
+		canciones = new ArrayList<>();
+		System.out.println("Listado Canciones");
+		System.out.println("---------------------------");
+		
+		//parsear el fichero XML
+		
+		DocumentBuilder dBuilder;
+		try {
+			 File file = new File(CANCIONES_XML);
+			 DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			 dBuilder = dbFactory.newDocumentBuilder();
+			 Document doc = dBuilder.parse(file);
+			 doc.getDocumentElement().normalize();
+			 //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		
+			NodeList nCanciones= doc.getElementsByTagName("cancion");
+			Canciones c= null;
+			Element element=null;
+			for(int i=0; i<nCanciones.getLength();i++) {
+				
+				
+				element=(Element)nCanciones.item(i);
+				//long id= element.getAttribute("id");
+				String nombre= element.getElementsByTagName("nombre").item(0).getTextContent();
+				
+				c=new Canciones(nombre, "", "");
+				canciones.add(c);
+			}
+		
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+		
+		//mostrar Cancion por pantalla
+		for(Canciones c: canciones) {
+			System.out.println("Cancion "+c.getId());
+			System.out.println("    Nombre:  "+c.getNombre());
+			System.out.println("    Artista: "+c.getArtista());
+			System.out.println("    Duracion: "+c.getDuracion());
+			
+			System.out.println("");
+		}
+		
+		
+		
+	}
+
+}
